851a72ac5cb52a5accc78a2fe07daf970c64ce49
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2018-01-10 22:30:00 +0530

    Develop page to create new travel plan.

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('travel/<int:pk>/', views.TravelDetailView.as_view(), name='travel_detail'),
+    path('travel/new/', views.TravelCreateView.as_view(), name='travel_new'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Travel
 
 
 class HomePageView(ListView):
     model = Travel
     template_name = 'travel.html'
+
+
+class TravelDetailView(DetailView):
+    model = Travel
+    template_name = 'travel_detail.html'
+
+
+class TravelCreateView(CreateView):
+    model = Travel
+    template_name = 'travel_new.html'
+    fields = '__all__'
